8df012440efed6260e3f5c5d9ef0bcb9f218d838
James Peou <47491266+catsinasippycup@users.noreply.github.com>
2024-05-10 19:47:26 -0700

    updated: contactMe email

--- portfolio/src/Components/ContactMe.jsx
+++ portfolio/src/Components/ContactMe.jsx
@@ -5,7 +5,7 @@ export default function ContactMe() {
         <div id="contact" className='bg-slate-700 p-10 m-20 max-w-lg mx-auto rounded-lg'>
             <h1 className='text-4xl text-white mb-6 text-center'>Contact Me</h1>
             <div className='text-center'>
-                <a href="mailto:your.email@example.com"
+                <a href="mailto:jbpeou@gmail.com"
                     className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer transition duration-300 ease-in-out transform hover:scale-105'>
                     Email
                 </a>
